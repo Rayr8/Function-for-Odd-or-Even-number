e1bcf17838bdd77d827f535cc2a095bcf4fdcd7c
raeesah <raeesahr8@gmail.com>
2021-06-18 12:44:50 +0200

    Created a function that proves a number is Odd or Even

---
+++ OddOrEven.js
@@ -0,0 +1,12 @@
+//Remainder
+function oddOrEven(x){
+    let test = x % 2;
+    if(test==0) {
+        console.log("It is an Even number"); 
+    } else {
+        console.log("It is an Odd number");
+    }
+}
+
+oddOrEven(12);
+oddOrEven(17);
